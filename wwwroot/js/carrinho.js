4ffcd0673a473884583e351376fbc175dd22236d
Ramon Cunha <ramon.network@hotmail.com>
2020-01-04 16:45:19 -0300

    add Script de decremento

---
+++ wwwroot/js/carrinho.js
@@ -0,0 +1,35 @@
+﻿class Carrinho {
+
+    clickIncremento(btn) {
+        let data = this.getData(btn);
+        data.Quantidade++;
+        this.postQuantidade(data);        
+    }
+
+    clickDecremento(btn) {
+        let data = this.getData(btn);
+        data.Quantidade--;
+        this.postQuantidade(data); 
+    }
+
+    getData(elemnt) {
+        var linhaDoItem = $(elemnt).parents('[item-id]');
+        var itemId = $(linhaDoItem).attr('item-id');
+        var novaQtd = $(linhaDoItem).find('input').val();
+        return {
+            Id: itemId,
+            Quantidade: novaQtd
+        };
+    }
+
+    postQuantidade(data) {
+        $.ajax({
+            url: '/pedido/updatequantidade',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(data)
+        });
+    }
+}
+
+var carrinho = new Carrinho();
